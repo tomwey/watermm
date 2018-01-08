23fdd8287101529b504d1310ef4cdd624a5402b9
tomwey <tomwey@163.com>
2018-01-08 16:22:45 +0800

    ccc

---
+++ src/pages/reserve/reserve.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ReservePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-reserve',
+  templateUrl: 'reserve.html',
+})
+export class ReservePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ReservePage');
+  }
+
+}
