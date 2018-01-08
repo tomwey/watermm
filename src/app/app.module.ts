cffb2b5f363bd291659dc590478558ae2d832942
tomwey <tomwey@163.com>
2018-01-08 16:22:33 +0800

    ccc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage }    from '../pages/home/home';
+import { ReservePage } from '../pages/reserve/reserve';
+import { WalletPage }  from '../pages/wallet/wallet';
+
+import { TabsPage }   from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,8 +15,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ReservePage,
+    WalletPage,
     HomePage,
     TabsPage
   ],
@@ -26,8 +27,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ReservePage,
+    WalletPage,
     HomePage,
     TabsPage
   ],
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ReservePage } from '../reserve/reserve';
+import { WalletPage } from '../wallet/wallet';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,10 +10,10 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = ReservePage;
+  tab3Root = WalletPage;
 
   constructor() {
-
+    
   }
 }
