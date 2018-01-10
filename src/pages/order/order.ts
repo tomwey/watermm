8b2ed18b82051f6ae66e5a90c1ffd8d25e874d0c
tomwey <tomwey@163.com>
2018-01-10 16:16:24 +0800

    ccc

--- src/pages/order/order.ts
+++ src/pages/order/order.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, App } from 'ionic-angular';
 
 /**
  * Generated class for the OrderPage page.
@@ -15,11 +15,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OrderPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    private alertCtrl: AlertController,
+    private app: App,
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderPage');
+  }
+
+  filter() {
+    let alert = this.alertCtrl.create();
+    // alert.setTitle('Lightsaber color');
+
+    alert.addInput({
+      type: 'radio',
+      label: '全部',
+      value: '0',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: '近一月',
+      value: '1',
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: '近三月',
+      value: '3',
+    });
+
+    alert.addButton('取消');
+    alert.addButton({
+      text: '确定',
+      handler: data => {
+        
+      }
+    });
+    alert.present();
+  }
+
+  buy() {
+    this.app.getRootNavs()[0].popToRoot();
+    let nav = this.app.getRootNavs()[0];
+    nav.getActiveChildNavs()[0].select(0);
+    // this.app.getActiveChildNavs()[0].select(page.index);
   }
 
 }
--- src/pages/water-ticket/water-ticket.ts
+++ src/pages/water-ticket/water-ticket.ts
@@ -15,6 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class WaterTicketPage {
 
+  useType: string = '0';
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
